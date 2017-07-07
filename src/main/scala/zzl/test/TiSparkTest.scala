8ddca8f10a7f5f665b9934162e3a7416b4a5d07f
WeichenXu <WeichenXu123@outlook.com>
2017-07-07 17:45:38 +0800

    test update

--- src/main/scala/zzl/test/TiSparkTest.scala
+++ src/main/scala/zzl/test/TiSparkTest.scala
@@ -15,8 +15,18 @@ object TiSparkTest {
       .appName("Spark SQL basic example")
       .getOrCreate()
     val ti = new TiContext(spark,List("192.168.14.71:2379","192.168.14.77:2379","192.168.14.82:2379"))//
+    //runT1Table(ti,spark)
+    runUserTable(ti,spark)
+
+  }
+  def runT1Table(ti:TiContext,spark:SparkSession): Unit ={
     val df = ti.tidbTable( "test", "t1")
     df.createGlobalTempView("t1")
     spark.sql("select avg(c1) from global_temp.t1").show
+  }
+  def runUserTable(ti:TiContext,spark:SparkSession): Unit ={
+    val df = ti.tidbTable( "test", "users")
+    df.createGlobalTempView("t1")
+    spark.sql("select avg(uid) from global_temp.t1").show
   }
 }
